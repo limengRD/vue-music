093df77bcefb5d3c390b681f1a5f5c89f547fb0a
limeng <limeng_fe@163.com>
2018-07-17 17:48:36 +0800

    轮播图

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -2,6 +2,13 @@
   <div class="recommend">
     <div class="recommend-content">
       <div class="slider-wrapper">
+        <mt-swipe :auto="4000">
+          <mt-swipe-item v-for="(item,index) in recommends" :key="index" >
+            <a :href="item.linkUrl">
+              <img class="my-swipeimg" :src="item.picUrl" alt="">
+            </a>
+          </mt-swipe-item>
+        </mt-swipe>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
@@ -16,14 +23,20 @@
 import {getRecommend} from '@/api/recommend'
 import {ERR_OK} from '@/api/config'
 export default {
+  data(){
+    return {
+      recommends:[]
+    }
+  },
   created() {
     this._getRecommend()
   },
   methods:{
     _getRecommend() {
       getRecommend().then((res) => {
+        console.log
         if(res.code === ERR_OK) {
-          console.log(res)
+          this.recommends = res.data.slider
         }
       })
     }
@@ -32,5 +45,10 @@ export default {
 </script>
 
 <style>
-
+  .mint-swipe {
+    height: 150px;
+  }
+  .my-swipeimg {
+    width: 100%;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,14 +5,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import '@/common/stylus/index.styl'
 
 //绑定在body 所有的标签都没有300毫秒的延迟
 fastclick.attach(document.body)
 
 Vue.config.productionTip = false
-
+Vue.use(MintUI)
 
 /* eslint-disable no-new */
 new Vue({
